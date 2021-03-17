002cabc8346fa57757f804c20dc3e5c78f19f416
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2021-03-17 15:29:05 -0300

    desafio finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+interface Props {
+  genres: Array<GenreResponseProps>;
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: Props) {
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
